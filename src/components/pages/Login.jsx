4fb2caf77f916e913ac9f34e25a97eba4006aa04
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-05-19 10:31:15 +0600

    login user done

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,44 +1,66 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "./Provider/Provider";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200 ">
       <div className="hero-content  w-96">
         <div className="card  shadow-2xl bg-base-100">
           <div className="card-body ">
             <h2 className="text-3xl font-semibold ">Please Login</h2>
-            <div className="form-control ">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                className="input input-bordered w-96"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                className="input input-bordered"
-              />
-
-              <div className="mt-3">
-                <small>Do not have Account? </small>
-                <Link to="/register" className=" underline text-blue-500">
-                  SignUp
-                </Link>
+            <form onSubmit={handleLogin}>
+              <div className="form-control ">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  name="email"
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered w-96"
+                />
               </div>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn hover:bg-cyan-300 border-none text-black bg-green-200">
-                Login
-              </button>
-            </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  name="password"
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+
+                <div className="mt-3">
+                  <small>Do not have Account? </small>
+                  <Link to="/register" className=" underline text-blue-500">
+                    SignUp
+                  </Link>
+                </div>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn hover:bg-cyan-300 border-none text-black bg-green-200">
+                  Login
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
